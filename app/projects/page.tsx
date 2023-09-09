72e123e44f36e1ebed5e8a511bbb53adf02a0e25
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2023-09-09 16:40:44 -0700

    projects page updates

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Script from "next/script";
+import Image from "next/image";
 
 export default function Projects() {
   const projects = [
@@ -9,19 +10,33 @@ export default function Projects() {
       description:
         "This is a simulator that allows you to visualize how different sorting algorithms run.",
       github: false,
+      pics: {
+        src: "/../../images/sortingSimPic.jpg",
+        width: 1000,
+        height: 500,
+        alt: "Picture of sorting simulator",
+        style: "py-2",
+      },
+    },
+    {
+      name: "Chess Timer App",
+      link: "https://github.com/TrevorStreng/mobileClock",
+      description:
+        "This is an app to keep time of people when they are playing chess. It is just a basic app made using React Native. It consists of two pages, the main timer page and a settings page where you can change the color for each player and a way to set the time to a custom time of the users choice.",
+      github: true,
+      pics: {
+        src: "/../../images/chessClockMain.PNG",
+        width: 82 * 2,
+        height: 179 * 2,
+        alt: "Picture of chess Clock app main screen",
+        style: "rounded-3xl py-2",
+      },
     },
     {
       name: "Robot Game",
       link: "https://github.com/TrevorStreng/Csc133",
       description:
         "This is the Robot Game we worked on in our Object Oriented Computer Graphics programming class. The goal is to get the player robot to all of the bases without running out of energy or taking too much damage from other robots or drones.",
-      github: true,
-    },
-    {
-      name: "Chess Timer",
-      link: "https://github.com/TrevorStreng/mobileClock",
-      description:
-        "This is a very simple React Native app built to keep time of each player in a chess game. There is two timers and once started the first playters timers will start counting down and when the screen is touched it will switch to the second players timer.",
       github: true,
     },
   ];
@@ -46,27 +61,48 @@ export default function Projects() {
         {projects.map((project, index) => (
           <div
             key={index}
-            className="text-center p-5 my-5 border border-blue-400 rounded-xl w-3/4 lg:max-w-xl"
+            className="text-center p-5 my-5 rounded-xl flex flex-col justify-center items-center"
           >
-            <h2 className="text-2xl">{project.name}</h2>
-            {/* <pic></pic> */}
-            <p className=" my-2">{project.description}</p>
-            {project.github ? (
-              <a
-                href={project.link}
-                target="_blank" // Open in a new tab
-                rel="noopener noreferrer" // Recommended for security
-                className={`text-xl hover:text-blue-400`}
+            <div className="max-w-[40rem]">
+              <h2 className="text-2xl p-4 border-y-4 border-blue-400">
+                {project.name}
+              </h2>
+              <p className="my-2">{project.description}</p>
+              {project.github ? (
+                <a
+                  href={project.link}
+                  target="_blank" // Open in a new tab
+                  rel="noopener noreferrer" // Recommended for security
+                  className={`text-xl hover:text-blue-400`}
+                >
+                  Click here to see on Github
+                </a>
+              ) : (
+                <Link
+                  href={project.link}
+                  className="text-xl p-4 hover:text-blue-400"
+                >
+                  Click here to test
+                </Link>
+              )}
+            </div>
+            {project.pics && (
+              <div
+                className="flex justify-center max-h-[25rem]"
+                // style={{
+                //   width: project.pics.width,
+                //   height: project.pics.height,
+                // }}
               >
-                Click here to see on Github
-              </a>
-            ) : (
-              <Link
-                href={project.link}
-                className="text-xl p-4 hover:text-blue-400"
-              >
-                Click here to test
-              </Link>
+                <Image
+                  src={project.pics.src}
+                  alt={project.pics.alt}
+                  // layout="responsive"
+                  width={project.pics.width}
+                  height={project.pics.height}
+                  className={project.pics.style}
+                ></Image>
+              </div>
             )}
           </div>
         ))}
