6e55d8346bd1c19f551fd099cdf854708c8459f3
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-05-15 18:30:10 -0700

    created a loading spinner while waiting for background image to load. Add loading component and added logic to main page

---
+++ components/loadingComp.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-screen flex justfiy-center items-center">
+      <div className="loading-spinner h-screen"></div>
+    </div>
+  );
+}
